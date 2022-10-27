4772c3d1514ddf36931f79b844614e555f5bff02
StoneT2000 <stonezt2019@gmail.com>
2022-10-27 12:26:19 -0700

    add rest of the data collection scripts

---
+++ scripts/abstract_trajectories/silo_obstacle_push/collect.sh
@@ -0,0 +1,4 @@
+# Generate training set (which is also the test set here)
+# note that we do not have access to the original environment so we have modified our boxpusher environment to be close to the original one proposed in SILO
+
+python scripts/abstract_trajectories/silo_obstacle_push/collect_obstacle_push.py
---
+++ scripts/abstract_trajectories/silo_pick_and_place/collect.sh
@@ -0,0 +1,4 @@
+# Generate training set (which is also the test set here)
+# note that we do not have access to the original environment so we have modified our blockstacking environment to be close to the original one proposed in SILO
+
+python scripts/abstract_trajectories/silo_pick_and_place/collect_dataset.py
